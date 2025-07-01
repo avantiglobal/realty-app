9063748d29fcaa150dcb1c22f6495332c0b837c5
Avanti Global LLC <avantiglobals@gmail.com>
2025-07-01 03:40:26 +0000

    Cambia el color de fondo de la barra superor a blanco y añade una sombra

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -110,7 +110,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
       </Sidebar>
 
       <SidebarInset>
-        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 py-2">
+        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 bg-card shadow-sm px-4 sm:static sm:h-auto sm:px-6 py-2">
             <SidebarTrigger className="sm:hidden" />
             <div className="ml-auto flex items-center gap-2">
                 <NotificationsNav />
