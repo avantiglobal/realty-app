2456a882f418ccad655edee53a07563be8a2ae85
Avanti Global LLC <avantiglobals@gmail.com>
2025-07-01 01:49:54 +0000

    Haz que la app use el light mode por defecto.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,8 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <UserProvider>
